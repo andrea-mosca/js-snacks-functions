ed75a4ab2bcd5027454dbbe4566b8a38777b9e71
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-08 14:43:47 +0200

    es 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function startingChar(nameList){
+    const outputArray = [];
+    for(let i = 0; i < nameList.length; i++){
+        const currentName = nameList[i];
+        outputArray.push(currentName[0]);
+    }
+    return outputArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(startingChar(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
