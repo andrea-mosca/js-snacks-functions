878274658fcda7fd5d29f6a16a3a2eea4a81daa7
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-08 15:21:25 +0200

    es4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function vowelsCount(string){
+    let vowelsNumber = 0;
+    for(let i = 0; i < string.length; i++){   
+        if(string[i] === "a"||
+            string[i] === "e"||
+            string[i] === "i"||
+            string[i] === "o"||
+            string[i] === "u"
+        ) vowelsNumber += 1;
+    }    
+    return vowelsNumber;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vowelsCount(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
